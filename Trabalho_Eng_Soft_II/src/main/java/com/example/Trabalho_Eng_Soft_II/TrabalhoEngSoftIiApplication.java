c689b8eaff0d51847b47868da2efe5cd1312d380
Gustavo Henrique <guazambujinha@gmail.com>
2026-04-22 20:33:25 -0300

    fist commit

---
+++ Trabalho_Eng_Soft_II/src/main/java/com/example/Trabalho_Eng_Soft_II/TrabalhoEngSoftIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trabalho_Eng_Soft_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoEngSoftIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoEngSoftIiApplication.class, args);
+	}
+
+}
---
+++ Trabalho_Eng_Soft_II/src/test/java/com/example/Trabalho_Eng_Soft_II/TrabalhoEngSoftIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trabalho_Eng_Soft_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoEngSoftIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
