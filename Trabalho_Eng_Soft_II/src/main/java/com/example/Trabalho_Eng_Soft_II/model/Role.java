f9ddfe6541f8115b64efb5a56828d29b2dbc3bd0
Daniel <daniel.glasenapp@edu.pucrs.br>
2026-05-02 15:59:09 -0300

    feat: implementa entidade User com BCrypt

---
+++ Trabalho_Eng_Soft_II/src/main/java/com/example/Trabalho_Eng_Soft_II/model/Role.java
@@ -0,0 +1,20 @@
+package com.example.Trabalho_Eng_Soft_II.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
