c6833e7053fcb1e10c2a908ab8d3e85be99830bf
RafaelReis22 <rafael@pucrs.br>
2026-05-11 15:41:01 -0300

    fix: resolve missing HttpStatus import in AuthController

--- Grupo-3/Trabalho_Eng_Soft_II/src/main/java/com/example/Trabalho_Eng_Soft_II/controller/AuthController.java
+++ Grupo-3/Trabalho_Eng_Soft_II/src/main/java/com/example/Trabalho_Eng_Soft_II/controller/AuthController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
